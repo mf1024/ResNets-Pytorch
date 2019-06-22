30a9a46c96c5347a7688fbed5aa0d148a4012b96
Martins Frolovs <martins.frolovs@gmail.com>
2019-06-22 17:52:49 +0300

    Fixes and improvements

--- main.py
+++ main.py
@@ -275,53 +275,62 @@ class ResNet(nn.Module):
         return x
 
 
+def plot_results(image_batch, predictions, truth, image_name = "plot"):
 
-model_resnet50 = ResNet(class_num = 10, is_bottleneck_resnet = True)
+    plot_columns = 4
+    plot_rows = image_batch.shape[0] // plot_columns
 
-data_path = "/Users/martinsf/data/images_1/imagenet_images/"
-dataset_train, dataset_test = get_imagenet_datasets(data_path, num_classes = 10)
+    img_count = plot_columns * plot_rows
 
-# data_loader_train = DataLoader(dataset_train, BATCH_SIZE, shuffle = True)
-#
-# import matplotlib.pyplot as plt
-#
-# for x in data_loader_train:
-#
-#
-#     print(x["image"].shape)
-#     y_prim = model_resnet50.forward(x["image"])
-#
-#     print(torch.sum(y_prim, dim=1))
-#     print(f"y_prim {y_prim}")
-#
-#     for i in range(BATCH_SIZE):
-#         img = x['image'][i].numpy()
-#         plt.title(x['class_name'][i])
-#         plt.imshow(np.transpose(img,(1,2,0)))
-#         plt.show()
-#
-#     break
+    fig, axes = plt.subplots(plot_rows, plot_columns, figsize=(plot_columns * 4, plot_rows * 3))
+    plt.subplots_adjust(hspace = 0.4)
 
+    for img_idx in range(img_count):
+        row = img_idx // plot_columns
+        col = img_idx % plot_columns
 
-NUM_CLASSES = dataset_train.get_number_of_classes()
+        img = image_batch[img_idx]
+        img = np.transpose(img,(1,2,0))
+
+        axes[row, col].imshow(img)
+
+        predicted_class = dataset_test.get_class_name(predictions[img_idx])
+        actual_class = dataset_test.get_class_name(truth[img_idx])
+        axes[row, col].set_title(f"Predicted class {predicted_class} \n but actually {actual_class}")
+
+    plt.savefig(f"{image_name}.jpg")
+
+NUM_CLASSES = None
+NUM_CLASSES = 10
+
+data_path = "/home/martin/ai/ImageNet-datasets-downloader/images_4/imagenet_images"
+dataset_train, dataset_test = get_imagenet_datasets(data_path, num_classes = NUM_CLASSES)
+
+if NUM_CLASSES == None:
+    NUM_CLASSES = dataset_train.get_number_of_classes()
+
 NUM_TRAIN_SAMPLES = dataset_train.get_number_of_samples()
 NUM_TEST_SAMPLES= dataset_test.get_number_of_samples()
 
 print(f"train_samples  {NUM_TRAIN_SAMPLES} test_samples {NUM_TEST_SAMPLES}")
 
 print(f'num_classes {NUM_CLASSES}')
-NUM_EPOCHS = 100
+NUM_EPOCHS = 10000
 BATCH_SIZE = 64
 LEARNING_RATE = 1e-4
-#DEVICE = 'cuda'
-DEVICE = 'cpu'
+DEVICE = 'cuda'
+#DEVICE = 'cpu'
 
-model_resnet50 = model_resnet50.to(DEVICE)
+model_resnet50 = ResNet(class_num = NUM_CLASSES, is_bottleneck_resnet = True).to(DEVICE)
 optimizer = torch.optim.Adam(params = model_resnet50.parameters(), lr = LEARNING_RATE)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f"number of resnet50 params {count_parameters(model_resnet50)}")
 
 data_loader_train = DataLoader(dataset_train, BATCH_SIZE, shuffle = True)
 data_loader_test = DataLoader(dataset_test, BATCH_SIZE, shuffle = True)
-
 
 for epoch in range(NUM_EPOCHS):
 
@@ -334,8 +343,7 @@ for epoch in range(NUM_EPOCHS):
     epoch_train_losses = []
     epoch_train_true_positives = 0
 
-    for batch in data_loader_train:
-
+    for batch_idx, batch in enumerate(data_loader_train):
 
         x = batch['image'].to(DEVICE)
         y = batch['cls'].to(DEVICE)
@@ -349,101 +357,55 @@ for epoch in range(NUM_EPOCHS):
 
         loss = torch.sum(-y_one_hot * torch.log(y_prim))
         epoch_train_losses.append(loss.detach().to('cpu').numpy())
-        # print(f"loss is {loss}")
 
         epoch_train_true_positives += torch.sum(y_prim.argmax(dim=1) == y)
-
-        # print(y_prim)
-
-        # print(f"Ground truthy {y}")
-        # print(f"Prediction {y_prim.argmax(dim=1)}")
-        # print(f"True positives {y == y_prim.argmax(dim=1)}")
 
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
 
-
-    epoch_accuracy = epoch_train_true_positives / NUM_TRAIN_SAMPLES
+    epoch_accuracy = float(epoch_train_true_positives) / float(NUM_TRAIN_SAMPLES)
+    print(f"true_positives {epoch_train_true_positives} from {NUM_TRAIN_SAMPLES} samples")
     print(f"Epoch {epoch} train mean loss is {np.mean(epoch_train_losses)}")
-    print(f"Epoch {epoch} train accuracy is {epoch_accuracy}")
+    print(f"Epoch {epoch} train accuracy is {epoch_accuracy*100} %")
 
     #TEST
-    model_resnet50 = model_resnet50.eval()
+    with torch.no_grad():
 
-    epoch_test_losses = []
-    epoch_test_true_positives = 0
+        model_resnet50 = model_resnet50.eval()
 
+        epoch_test_losses = []
+        epoch_test_true_positives = 0
 
+        for batch_idx, batch in enumerate(data_loader_test):
 
-    def plot_results(image_batch, predictions, truth, image_name = "plot"):
+            x = batch['image'].to(DEVICE)
+            y = batch['cls'].to(DEVICE)
 
-        plot_columns = 4
-        plot_rows = image_batch.shape[0] // plot_columns
+            y_one_hot = torch.zeros(x.shape[0], NUM_CLASSES).to(DEVICE)
+            y_one_hot = y_one_hot.scatter_(1, y.unsqueeze(dim=1), 1)
 
-        img_count = plot_columns * plot_rows
+            labels = batch['class_name']
 
-        fig, axes = plt.subplots(plot_rows, plot_columns, figsize=(plot_columns * 4, plot_rows * 3))
-        plt.subplots_adjust(hspace = 0.4)
+            y_prim = model_resnet50.forward(x)
+            loss = torch.sum(-y_one_hot * torch.log(y_prim))
+            epoch_test_losses.append(loss.detach().to('cpu').numpy())
 
-        for img_idx in range(img_count):
-            row = img_idx // plot_columns
-            col = img_idx % plot_columns
+            epoch_test_true_positives += torch.sum(y_prim.argmax(dim=1) == y)
 
-            img = image_batch[img_idx]
-            img = np.transpose(img,(1,2,0))
-
-            axes[row, col].imshow(img)
-
-            predicted_class = dataset_test.get_class_name(predictions[img_idx])
-            actual_class = dataset_test.get_class_name(truth[img_idx])
-            axes[row, col].set_title(f"Predicted class {predicted_class} \n but actually {actual_class}")
-
-        plt.savefig(f"{image_name}.jpg")
-
-    for batch_idx, batch in enumerate(data_loader_test):
-
-        x = batch['image'].to(DEVICE)
-        y = batch['cls'].to(DEVICE)
-
-        y_one_hot = torch.zeros(x.shape[0], NUM_CLASSES).to(DEVICE)
-        y_one_hot = y_one_hot.scatter_(1, y.unsqueeze(dim=1), 1)
-
-        labels = batch['class_name']
-
-        y_prim = model_resnet50.forward(x)
-
-        loss = torch.sum(-y_one_hot * torch.log(y_prim))
-        epoch_test_losses.append(loss.detach().to('cpu').numpy())
-        # print(f"loss is {loss}")
-
-        epoch_test_true_positives += torch.sum(y_prim.argmax(dim=1) == y)
-
-        # print(y_prim)
-        #
-        # print(f"Ground truthy {y}")
-        # print(f"Prediction {y_prim.argmax(dim=1)}")
-        # print(f"True positives {y == y_prim.argmax(dim=1)}")
-
-        if batch_idx == 0:
-            plot_results(x.detach().to('cpu').numpy(),
-                         y_prim.argmax(dim=1).detach().to('cpu').numpy(),
-                         y.detach().to('cpu').numpy(),
-                         image_name=f"epoch_{epoch}")
+            if batch_idx == 0:
+                plot_results(x.detach().to('cpu').numpy(),
+                             y_prim.argmax(dim=1).detach().to('cpu').numpy(),
+                             y.detach().to('cpu').numpy(),
+                             image_name=f"epoch_{epoch}")
 
 
 
-    epoch_accuracy = epoch_test_true_positives / NUM_TEST_SAMPLES
-    print(f"Epoch {epoch} test mean loss is {np.mean(epoch_test_losses)}")
-    print(f"Epoch {epoch} test accuracy is {epoch_accuracy}")
+        epoch_accuracy = float(epoch_test_true_positives) / float(NUM_TEST_SAMPLES)
+        print(f"true_positives {epoch_test_true_positives} from {NUM_TEST_SAMPLES} samples")
+        print(f"Epoch {epoch} test mean loss is {np.mean(epoch_test_losses)}")
+        print(f"Epoch {epoch} test accuracy is {epoch_accuracy * 100}")
 
-
-
-
-
-
-
-
-#TODO: SGD
+    #TODO: SGD
 
